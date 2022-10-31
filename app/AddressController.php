ed0452d63feaeef617db86ad2fb6da094263717e
laloparkour <laloparkour09@gmail.com>
2022-10-30 18:08:57 -0700

    Modulo Direcciones - Modificación al controlador

--- app/AddressController.php
+++ app/AddressController.php
@@ -54,17 +54,17 @@ class AddressController {
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://crud.jonathansoto.mx/api/coupons',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => '',
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 0,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => 'GET',
-            CURLOPT_HTTPHEADER => array(
-                'Authorization: Bearer ' . $_SESSION['token'],
-            ),
+          CURLOPT_URL => 'https://crud.jonathansoto.mx/api/addresses/clients/1',
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => '',
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => 'GET',
+          CURLOPT_HTTPHEADER => array(
+            'Authorization: Bearer ' . $_SESSION['token'],
+          ),
         ));
 
         $response = curl_exec($curl);
