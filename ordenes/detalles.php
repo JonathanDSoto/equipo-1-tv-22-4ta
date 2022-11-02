2608de5d3ef320afa53ec417814e6670f494ebaa
Racheth <racheth14@gmail.com>
2022-11-02 02:18:46 -0700

    Orders - Seguridad implementada

--- ordenes/detalles.php
+++ ordenes/detalles.php
@@ -1,5 +1,6 @@
 <?php
 include_once "../app/config.php";
+include "../layouts/Authentication.templade.php";
 ?>
 <!DOCTYPE html>
 <html>
--- ordenes/index.php
+++ ordenes/index.php
@@ -1,5 +1,6 @@
 <?php
 	include_once "../app/config.php";
+	include "../layouts/Authentication.templade.php";
     include("../app/OrdersController.php");
     include("../app/ClientsController.php");
     include("../app/productController.php");
